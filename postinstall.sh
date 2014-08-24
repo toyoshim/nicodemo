a7194ffb99c34e7f595314cbe44021c96db505b2
Takashi Toyoshima <toyoshim@gmail.com>
2014-08-24 06:26:05 +0000

    3d art

--- postinstall.sh
+++ postinstall.sh
@@ -2,7 +2,7 @@
 
 cd static
 mkdir -p tmalib
-for rev in 91903e03995a 4ec2172918a6 c43364aa64f3; do
+for rev in 91903e03995a 4ec2172918a6 c43364aa64f3 b9198258d2b9; do
   git clone https://code.google.com/p/tmalib/ tmalib/$rev && (cd tmalib/$rev; git checkout $rev)
 done
 
