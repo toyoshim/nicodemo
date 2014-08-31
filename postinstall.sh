9defdaa4f8d1e5171f413e6643f365d5416bd836
Takashi Toyoshima <toyoshim@gmail.com>
2014-08-31 18:33:06 +0000

    update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,10 @@
 cd static
 mkdir -p tmalib
 for rev in 91903e03995a 4ec2172918a6 c43364aa64f3 b9198258d2b9 1e344ef9f97c; do
-  git clone https://code.google.com/p/tmalib/ tmalib/$rev && (cd tmalib/$rev; git checkout $rev)
+  git clone https://code.google.com/p/tmalib/ tmalib/$rev
+  cd tmalib/$rev
+  git checkout $rev
+  cd ../..
 done
 
 exit 0
