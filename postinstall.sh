cac4f39d3aae148414bfa72f17f16a58dac67fec
Takashi Toyoshima <toyoshim@gmail.com>
2014-08-31 18:51:10 +0000

    retry

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,10 @@
 cd static
 mkdir -p tmalib
 for tag in nico nico2 nico3 nico4 nico5 nico6; do
-  git clone https://code.google.com/p/tmalib/ -b $tag tmalib/$tag
+  git clone https://code.google.com/p/tmalib/ tmalib/$tag
+  cd tmalib/$tag
+  git checkout $tag
+  cd ../..
 done
 
 exit 0
