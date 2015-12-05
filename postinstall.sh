e2992be5847de14eb69bbd1a5045c68a4467a18d
Takashi Toyoshima <toyoshim@gmail.com>
2015-12-05 20:26:52 +0000

    add nico8

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,8 @@
 ./clean.sh
 cd static
 mkdir -p tmalib
-for branch in nico nico2 nico3 nico4 nico5 nico6 nico7; do
-  git clone https://code.google.com/p/tmalib/ -b $branch tmalib/$branch
+for branch in nico nico2 nico3 nico4 nico5 nico6 nico7 nico8; do
+  git clone https://github.com/toyoshim/tmalib.git -b $branch tmalib/$branch
 done
 
 exit 0
