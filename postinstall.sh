961a5df94425a2f9bf511e46906dda117e816d0a
Takashi Toyoshima <toyoshim@gmail.com>
2022-09-26 05:05:46 +0900

    reorg for GHP

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./clean.sh
-cd static
-mkdir -p tmalib
 for branch in nico nico2 nico3 nico4 nico5 nico6 nico7 nico8; do
-  git clone https://github.com/toyoshim/tmalib.git -b $branch tmalib/$branch
+  git submodule add https://github.com/toyoshim/tmalib.git tmalib/$branch
+  cd tmalib/$branch
+  git checkout $branch
+  cd ../..
 done
 
 exit 0
